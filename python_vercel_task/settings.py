5a9ca9874449c3bac75ca275c54e4d08416f4730
Tu nombre <anapaulazorrilla73@gmail.com>
2024-11-20 11:53:54 +0100

    primer commit

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from os import path
-from decouple import  config
+from decouple import  config # type: ignore
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.rfimefwcilimljmygcve',
+        'USER': 'postgres.yzxvphonbhjojzroyvtg',
         'PASSWORD': config("SUPABASE_PASSWORD"),
         'HOST': config("SUPABASE_HOST"),
         'PORT': '6543',
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
